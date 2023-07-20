8d7c6dc5134c1f82908f1919d899c6e53a9fa40c
getenesh <getenesh.guta@gmail.com>
2023-07-20 13:01:34 -0400

    debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
+
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_path, notice: 'Logged in successfully.'
