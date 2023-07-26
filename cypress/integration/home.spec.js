d46a0ebea84c5b2d4c7b417cd6e2be79bd3c8baf
getenesh <getenesh.guta@gmail.com>
2023-07-26 11:32:19 -0400

    home page test

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,16 @@
+
+
+describe('Jungle App Home Page', () => {
+  it('Visits the home page and checks the title section', () => {
+    cy.visit('http://localhost:3000/'); 
+    cy.get('section.title').should('be.visible'); 
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 12 products on the page", () => {
+    cy.get(".products article").should("have.length", 12);
+  });
+});
